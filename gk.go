a769f04706bf80bff5bbdc4f682c0096a69c53de
Damian Gryski <damian@gryski.com>
2014-06-11 16:56:37 +0200

    initial import

---
+++ gk.go
@@ -0,0 +1,78 @@
+package gk
+
+import "math"
+
+type GKStream struct {
+	summary *skiplist
+	epsilon float64
+	n       int
+}
+
+type tuple struct {
+	v     float64
+	g     int
+	delta int
+}
+
+func NewGK(epsilon float64) *GKStream {
+	return &GKStream{
+		epsilon: epsilon,
+		summary: newSkiplist(),
+	}
+}
+
+func (s *GKStream) Insert(v float64) {
+
+	value := tuple{v, 1, 0}
+
+	elt := s.summary.Insert(value)
+
+	if elt.prev[0] != s.summary.head && elt.next[0] != nil {
+		//elt.value.delta = int(math.Floor(2 * s.epsilon * float64(s.n)))
+		elt.value.delta = elt.next[0].value.g + elt.next[0].value.delta - 1
+	}
+
+	s.n++
+	if s.n%int(1.0/float64(2.0*s.epsilon)) == 0 {
+		s.compress()
+	}
+}
+
+func (s *GKStream) compress() {
+
+	for elt := s.summary.head.next[0]; elt != nil && elt.next[0] != nil; {
+		next := elt.next[0]
+		t := elt.value
+		nt := &next.value
+		if t.g+nt.g+nt.delta <= int(math.Floor(2*s.epsilon*float64(s.n))) {
+			nt.g += t.g
+			s.summary.Remove(elt)
+		}
+		elt = next
+	}
+}
+
+func (s *GKStream) Query(q float64) float64 {
+
+	// convert quantile to rank
+
+	r := int(q * float64(s.n))
+
+	var rmin int
+
+	for elt := s.summary.head.next[0]; elt.next[0] != nil; elt = elt.next[0] {
+
+		t := elt.value
+
+		rmin += t.g
+		rmax := rmin + t.delta
+
+		if r-rmin <= int(s.epsilon*float64(s.n)) && rmax-r <= int(s.epsilon*float64(s.n)) {
+			return t.v
+		}
+	}
+
+	// panic("not reached")
+
+	return 0
+}
---
+++ skiplist.go
@@ -0,0 +1,83 @@
+package gk
+
+import "math/rand"
+
+const maxHeight = 31
+
+type skiplist struct {
+	height int
+	head   *node
+}
+
+type node struct {
+	value tuple
+	next  []*node
+	prev  []*node
+}
+
+func newSkiplist() *skiplist {
+	return &skiplist{
+		height: 0,
+		head:   &node{next: make([]*node, maxHeight)},
+	}
+}
+
+func (s *skiplist) Insert(t tuple) *node {
+	level := 0
+
+	n := rand.Int31()
+	for n&1 == 1 {
+		level++
+		n >>= 1
+	}
+
+	if level > s.height {
+		s.height++
+		level = s.height
+	}
+
+	node := &node{
+		value: t,
+		next:  make([]*node, level+1),
+		prev:  make([]*node, level+1),
+	}
+	curr := s.head
+	for i := s.height; i >= 0; i-- {
+
+		for curr.next[i] != nil && t.v >= curr.next[i].value.v {
+			curr = curr.next[i]
+		}
+
+		if i > level {
+			continue
+		}
+
+		node.next[i] = curr.next[i]
+		if curr.next[i] != nil && curr.next[i].prev[i] != nil {
+			curr.next[i].prev[i] = node
+		}
+		curr.next[i] = node
+		node.prev[i] = curr
+	}
+
+	return node
+}
+
+func (s *skiplist) Remove(node *node) {
+
+	// remove n from each level of the skiplist
+
+	for i := range node.next {
+		prev := node.prev[i]
+		next := node.next[i]
+
+		if prev != nil {
+			prev.next[i] = next
+		}
+		if next != nil {
+			next.prev[i] = prev
+		}
+		node.next[i] = nil
+		node.prev[i] = nil
+	}
+}
